105516b192a360d9e172205a003a609f9da67bde
Roger Curley <rocurley@gmail.com>
2017-09-10 17:18:44 -0700

    saving wordlists

--- src/main.rs
+++ src/main.rs
@@ -92,11 +92,25 @@ fn main() {
   dims.sort_by_key(|&(x,y)| -((x*y) as i64));
   for &(&w,&h) in dims.iter(){
     let empty = Array::from_elem((h,w), None);
+    let mut row_matches = Vec::new();
+    for _ in 0..h {row_matches.push(Unconstrained)};
+    let mut col_matches = Vec::new();
+    for _ in 0..w {col_matches.push(Unconstrained)};
+    let start = WordRectangle {
+      array: empty,
+      row_matches : row_matches,
+      col_matches : col_matches,
+    };
     println!("{}x{}", w, h);
-    let mut stack = vec![empty];
-    let mut pb_tuple : Option<(ProgressBar<_>, u64)> = None;
+    //let mut stack = vec![empty];
+    //let mut pb_tuple : Option<(ProgressBar<_>, u64)> = None;
     PROFILER.lock().unwrap().start(format!("profiling/{}x{}.profile", w,h)).unwrap();
     let start_time = Instant::now();
+    match step_word_rectangle(& indices, & words_by_length, start, true) {
+      None => println!("No rectangle found"),
+      Some(rect) => println!("Found:\n{}", show_word_rectangle(& rect)),
+    }
+    /*
     loop {
       match stack.pop() {
         None => {
@@ -128,6 +142,7 @@ fn main() {
         },
       };
     };
+    */
     let elapsed = start_time.elapsed();
     println!("{:?}", elapsed);
     PROFILER.lock().unwrap().stop().unwrap();
@@ -140,14 +155,43 @@ fn ix(pos : usize, c : char) -> usize {
 }
 
 #[derive(Debug)]
-enum WordsMatch <'w>{
+enum WordsMatch <'a, 'w : 'a>{
   Unconstrained,
   Filled,
-  Matches { matches: Vec<& 'w[char]>}
+  OwnedMatches { matches: Vec<& 'w[char]>},
+  BorrowedMatches { matches: & 'a Vec<& 'w[char]>},
 }
 use WordsMatch::*;
 
-impl<'a> Ord for WordsMatch<'a> {
+impl <'a, 'w : 'a> WordsMatch<'a, 'w> {
+  fn shallow_copy<'b>(& 'b self) -> WordsMatch<'b, 'w> {
+    match *self {
+      Unconstrained => Unconstrained,
+      Filled => Filled,
+      OwnedMatches{ matches: ref old_matches} => BorrowedMatches {matches: old_matches},
+      BorrowedMatches{ matches : old_matches} => OwnedMatches {matches: old_matches.clone()},
+    }
+  }
+
+  fn apply_constraint<'b : 'a>(& mut self, index : &'b Vec<Vec<& 'w[char]>>
+    , pos : usize
+    , c : char) {
+    match *self {
+      Unconstrained => *self = BorrowedMatches{matches: & index[ix(pos, c)]},
+      OwnedMatches {ref mut matches} => {
+        matches.retain(|word| word[pos] == c);
+      },
+      BorrowedMatches { matches: old_matches} => {
+        let mut new_matches = old_matches.clone();
+        new_matches.retain(|word| word[pos] == c);
+        *self = OwnedMatches { matches : new_matches}
+      },
+      Filled => {}
+    }
+  }
+}
+
+impl<'a, 'w> Ord for WordsMatch<'a, 'w> {
   fn cmp(&self, other: & WordsMatch) -> Ordering {
     match(self, other) {
       (& Filled, & Filled) => Ordering::Equal,
@@ -156,18 +200,21 @@ impl<'a> Ord for WordsMatch<'a> {
       (& Unconstrained, & Unconstrained) => Ordering::Equal,
       (& Unconstrained, _) => Ordering::Greater,
       (_, & Unconstrained) => Ordering::Less,
-      (& Matches{matches: ref l}, & Matches{matches: ref r}) => l.len().cmp(& r.len()),
+      (& OwnedMatches{matches: ref l}, & OwnedMatches{matches: ref r}) => l.len().cmp(& r.len()),
+      (& BorrowedMatches{matches: l}, & OwnedMatches{matches: ref r}) => l.len().cmp(& r.len()),
+      (& OwnedMatches{matches: ref l}, & BorrowedMatches{matches: r}) => l.len().cmp(& r.len()),
+      (& BorrowedMatches{matches: l}, & BorrowedMatches{matches: r}) => l.len().cmp(& r.len()),
     }
   }
 }
 
-impl<'a> PartialOrd for WordsMatch<'a> {
+impl<'a, 'w> PartialOrd for WordsMatch<'a, 'w> {
   fn partial_cmp(&self, other: &WordsMatch) -> Option<Ordering> {
     Some(self.cmp(other))
   }
 }
 
-impl<'a> PartialEq for WordsMatch<'a> {
+impl<'a, 'w> PartialEq for WordsMatch<'a, 'w> {
   fn eq(&self, other: & WordsMatch) -> bool {
     match self.cmp(other) {
       Ordering::Equal => true,
@@ -176,31 +223,22 @@ impl<'a> PartialEq for WordsMatch<'a> {
   }
 }
 
-impl<'a> Eq for WordsMatch<'a> {}
+impl<'a, 'w> Eq for WordsMatch<'a, 'w> {}
 
-fn fit_word<'a, 'b, 'w, I>(to_fill : I, index : &'b Vec<Vec<& 'w[char]>>) -> WordsMatch<'w> where
-  I : Iterator<Item = & 'a Option<char>>{
-  let mut full = true;
-  let mut constraints : Vec<(usize, char)> = Vec::new();
-  for (pos,&c_option) in to_fill.enumerate() {
-    match c_option {
-      Some(c) => {
-        constraints.push((pos, c));
-      }
-      None => full = false
-    };
-  };
-  constraints.sort_by_key(|&(pos, c)| index[ix(pos, c)].len());//Strictest first
-  match (constraints.split_first(), full) {
-    (_, true) => WordsMatch::Filled,
-    (None, false) => WordsMatch::Unconstrained,
-    (Some((&(seed_pos, seed_c), tail_constraints)), false) => {
-      let mut matches : Vec<& 'w[char]> = index[ix(seed_pos, seed_c)].clone();
-      for &(pos, c) in tail_constraints{
-        matches.retain(|word| word[pos] == c);
-      }
-      WordsMatch::Matches{matches : matches}
-    },
+#[derive(Debug)]
+struct WordRectangle<'a, 'w : 'a> {
+  array : Array2<Option<char>>,
+  row_matches : Vec<WordsMatch<'a, 'w>>,
+  col_matches : Vec<WordsMatch<'a, 'w>>,
+}
+
+impl <'a, 'w> WordRectangle<'a, 'w> {
+  fn shallow_copy<'b>(& 'b self) -> WordRectangle<'b, 'w>{
+    WordRectangle{
+      array : self.array.clone(),
+      row_matches : self.row_matches.iter().map(|m| m.shallow_copy()).collect(),
+      col_matches : self.col_matches.iter().map(|m| m.shallow_copy()).collect(),
+    }
   }
 }
 
@@ -211,80 +249,86 @@ fn show_word_rectangle(word_rectangle : & Array2<Option<char>>) -> String {
   join(rows,"\n")
 }
 
-fn complete_word_rectangle(word_rectangle : & Array2<Option<char>>) -> bool {
-  word_rectangle.iter().all(|x| x.is_some())
-}
-
 fn step_word_rectangle<'a, 'w>(
   indices : & HashMap<usize, Vec<Vec<& 'w[char]>>>,
   words_by_length : & 'a HashMap<usize, Vec<& 'w[char]>>,
-  word_square : Array2<Option<char>>
-  ) -> Box<Iterator<Item = Array2<Option<char>>> + 'a>
+  word_rectangle : WordRectangle<'a, 'w>,
+  show_pb : bool,
+  ) -> Option<Array2<Option<char>>>
   {
-  let width = word_square.shape()[1];
-  let height = word_square.shape()[0];
-  //println!("{}x{}", width, height);
+  let width = word_rectangle.array.shape()[1];
+  let height = word_rectangle.array.shape()[0];
   let unfiltered_row_candidates = &words_by_length[&width];
   let unfiltered_col_candidates = &words_by_length[&height];
 
-  let row_candidates = word_square.map_axis(Axis(1), |row|
-    fit_word(row.iter(), &indices[&width])
-  );
-  let col_candidates = word_square.map_axis(Axis(0), |col|
-    fit_word(col.iter(), &indices[&height])
-  );
-  /*
-  println!("Expanding:\n{}", show_word_rectangle(& word_square));
-  println!("Row Candidates");
-  for r in row_candidates.iter(){
-    println!("{:?}", r);
-  };
-  println!("Col Candidates");
-  for r in col_candidates.iter(){
-    println!("{:?}", r);
-  };
-  */
   let (best_row_ix, best_row_matches) =
-    row_candidates.into_raw_vec().into_iter().enumerate()
+    word_rectangle.row_matches.iter().enumerate()
       .min_by(|& (_, ref l_matches), & (_, ref r_matches)| l_matches.cmp(r_matches)).expect("Empty rows");
   let (best_col_ix, best_col_matches) =
-    col_candidates.into_raw_vec().into_iter().enumerate()
+    word_rectangle.col_matches.iter().enumerate()
       .min_by(|& (_, ref l_matches), & (_, ref r_matches)| l_matches.cmp(r_matches)).expect("Empty cols");
   if (& best_row_matches, unfiltered_row_candidates.len()) <
     (& best_col_matches, unfiltered_col_candidates.len()) {
-      //println!("Adding row");
-      let matches : Vec<& [char]> = match best_row_matches {
-        Filled => panic!("Ran step_word_rectangle on full rectangle"),
-        Unconstrained => unfiltered_row_candidates.iter().cloned().collect(),
-        Matches{matches:m} => m,
+      let matches : & [& [char]] = match *best_row_matches {
+        Filled => return Some(word_rectangle.array),
+        Unconstrained => unfiltered_row_candidates,
+        OwnedMatches{matches:ref m} => m,
+        BorrowedMatches{matches:m} => m,
       };
-      Box::new(matches.into_iter().map(move |word| {
-        let mut new_square = word_square.clone();
+      let mut pb = if show_pb {
+        Some(ProgressBar::new(matches.len() as u64))
+      } else {
+        None
+      };
+      for & word in matches {
+        for p in & mut pb {p.inc();};
+        let mut new_rectangle = word_rectangle.shallow_copy();
+        new_rectangle.row_matches[best_row_ix] = Filled;
         {
-          let target = new_square.subview_mut(Axis(0),best_row_ix);
+          let target = new_rectangle.array.subview_mut(Axis(0),best_row_ix);
           Zip::from(word)
             .and(target)
             .apply(|& word_c, target_slot| *target_slot = Some(word_c));
-        };
-        new_square
-      }))
+          Zip::from(word)
+            .and(& mut new_rectangle.col_matches)
+            .apply(|&c, col| col.apply_constraint( &indices[&height], best_row_ix, c));
+        }
+        let child_result = step_word_rectangle(indices, words_by_length, new_rectangle, false);
+        if child_result.is_some() {
+          return child_result;
+        }
+      }
   } else {
-      //println!("Adding col");
-      let matches : Vec<& [char]> = match best_col_matches {
-        Filled => panic!("Ran step_word_rectangle on full rectangle"),
-        Unconstrained => unfiltered_col_candidates.iter().cloned().collect(),
-        Matches{matches:m} => m,
+      let matches : & [& [char]] = match *best_col_matches {
+        Filled => return Some(word_rectangle.array),
+        Unconstrained => unfiltered_col_candidates,
+        OwnedMatches{matches:ref m} => m,
+        BorrowedMatches{matches:m} => m,
       };
-      Box::new(matches.into_iter().map(move |word| {
-        let mut new_square = word_square.clone();
+      let mut pb = if show_pb {
+        Some(ProgressBar::new(matches.len() as u64))
+      } else {
+        None
+      };
+      for & word in matches {
+        for p in & mut pb {p.inc();};
+        let mut new_rectangle = word_rectangle.shallow_copy();
+        new_rectangle.col_matches[best_col_ix] = Filled;
         {
-          let target = new_square.subview_mut(Axis(1),best_col_ix);
+          let target = new_rectangle.array.subview_mut(Axis(1),best_col_ix);
           Zip::from(word)
             .and(target)
             .apply(|& word_c, target_slot| *target_slot = Some(word_c));
-        };
-        new_square
-      }))
+          Zip::from(word)
+            .and(& mut new_rectangle.row_matches)
+            .apply(|&c, row| row.apply_constraint( &indices[&width], best_col_ix, c));
+        }
+        let child_result = step_word_rectangle(indices, words_by_length, new_rectangle, false);
+        if child_result.is_some() {
+          return child_result;
+        }
+      }
   }
+  None
 }
 
